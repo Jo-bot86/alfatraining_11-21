cfff54066371511aac29a5036897b01a1220b783
Armin Fröhlich <mail@arminfroehlich.de>
2021-01-08 12:08:59 +0100

    A3, add BookDetails, add useState

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,25 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useState} from 'react';
 
 import BookList from './BookList'
+import BookDetails from './BookDetails'
+import Book from '../types/Book'
 
 export default function App(): ReactElement {
+  const [book, setBook] = useState<Book>();
+
+  const showList = () => {
+    setBook(undefined)
+  };
+
+  const showDetails = (book_: Book) => {
+    setBook(book_)
+  };
+
   return (
     <div className="ui container">
-      <BookList />
+      {book
+        ? <BookDetails showList={showList} book={book} />
+        : <BookList showDetails={showDetails} />}
     </div>
-  );
+  )
 }
