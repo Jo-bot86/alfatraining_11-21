741c6ce844a427c4a8f62b0bc5a9a912d4157074
Armin Fröhlich <mail@arminfroehlich.de>
2021-09-30 17:01:27 +0200

    add msw

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  ReactDOM.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    document.getElementById('root'),
+  );
+};
+
+if (process.env.NODE_ENV === 'development') {
+  import('./mocks/browser').then(({worker}) => {
+    worker.start();
+    render();
+  });
+} else {
+  render();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
