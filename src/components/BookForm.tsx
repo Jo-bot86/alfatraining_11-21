gmiDash/alfatraining_12-20

A8, add html validation for BookForm, add styles

src/components/BookForm.tsx
@@ -1,6 +1,6 @@
 import React, {useState, SyntheticEvent} from 'react';
 import {useHistory} from 'react-router-dom'
-// import './BookForm.module.css'
+import css from './BookForm.module.css'
 
 import {bookApi} from '../shared/BookApi'
 
@@ -71,25 +71,35 @@ export default function BookForm(): JSX.Element {
   }
 
   return (
-    <form className="ui form" onSubmit={handleSubmit}>
+    <form className={`ui form ${css.bookFom}`} onSubmit={handleSubmit}>
       <label>Buchtitel</label>
-      <input placeholder="Titel" value={title} onChange={(e) => {setTitle(e.target.value)}} />
+      <input placeholder="Titel" required value={title} onChange={(e) => {setTitle(e.target.value)}} />
 
       <label>Untertitel</label>
       <input placeholder="Subtitle" value={subtitle} onChange={(e) => {setSubtitle(e.target.value)}} />
 
       <label>Isbn</label>
-      <input placeholder="Isbn" value={isbn} onChange={(e) => {setIsbn(e.target.value)}} />
+      <input
+        placeholder="Isbn"
+        required
+        pattern="\d{9}|\d{11}"
+        title="Isbn Nummer muss zwischen 9 und 11 Zeichen lang sein"
+        value={isbn}
+        onChange={(e) => {setIsbn(e.target.value)}} />
 
       <label>Erscheinungsdatum</label>
-      <input type="date" value={published} onChange={(e) => {setPublished(e.target.value)}} />
+      <input type="date" required value={published} onChange={(e) => {setPublished(e.target.value)}} />
 
       <label>Authoren</label>
       <button className="ui mini button" type="button" onClick={onAddAuthor}>+</button>
       <button className="ui mini button" type="button" onClick={onRemoveAuthor}>-</button>
       {authors.map((author, index) =>
         <div key={index} className="sixteen wide field">
-          <input placeholder="Autor" value={author} onChange={(e) => {onChangeAuthor(e.target.value, index)}} />
+          <input
+            placeholder="Autor"
+            required
+            value={author}
+            onChange={(e) => {onChangeAuthor(e.target.value, index)}} />
         </div>
       )}
 
@@ -101,7 +111,7 @@ export default function BookForm(): JSX.Element {
       <button className="ui mini button" type="button" onClick={onRemoveThumbnail}>-</button>
       {thumbnails.map((thumbnail, index) =>
         <div key={index} className="field">
-          <input placeholder="Url" className="nine wide field" value={thumbnail.url}
+          <input placeholder="Url" required className="nine wide field" value={thumbnail.url}
             onChange={(e) => {onChangeThumbnail(index, {url: e.target.value})}} />
           <input placeholder="Titel" className="seven wide field" value={thumbnail.title}
             onChange={(e) => {onChangeThumbnail(index, {title: e.target.value})}} />

Jo-bot86/alfatraining_11-21

src/components/BookForm.tsx
@@ -1,5 +1,6 @@
 import React, {useState, SyntheticEvent} from 'react';
 import {useHistory} from 'react-router-dom'
+import css from './BookForm.module.css'
 
 import {bookApi} from '../shared/BookApi'
 
@@ -8,10 +9,10 @@ export default function BookForm(): JSX.Element {
 
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
-  const [isbn, setIsbn] = useState(Math.floor(Math.random() * 99999999999 + 111111111).toString())
+  const [isbn, setIsbn] = useState(Math.floor(Math.random() * 8999999999 + 1111111111).toString())
   const [description, setDescription] = useState('')
   const [authors, setAuthors] = useState([''])
-  const [thumbnails, setThumbnails] = useState([buildThumbnail('title', 'https://ng-buch.de/public/monkey-thinking.svg')])
+  const [thumbnails, setThumbnails] = useState([buildThumbnail('', 'https://ng-buch.de/public/monkey-thinking.svg')])
   const [published, setPublished] = useState('')
 
   const history = useHistory()
@@ -66,18 +67,24 @@ export default function BookForm(): JSX.Element {
   }
 
   return (
-    <form className="ui form" onSubmit={handleSubmit}>
+    <form className={`ui form ${css.bookForm}`} onSubmit={handleSubmit}>
       <label>Buchtitel</label>
-      <input placeholder="Titel" value={title} onChange={(e) => {setTitle(e.target.value)}} />
+      <input placeholder="Titel" required value={title} onChange={(e) => {setTitle(e.target.value)}} />
 
       <label>Untertitel</label>
       <input placeholder="Subtitle" value={subtitle} onChange={(e) => {setSubtitle(e.target.value)}} />
 
       <label>Isbn</label>
-      <input placeholder="Isbn" value={isbn} onChange={(e) => {setIsbn(e.target.value)}} />
+      <input
+        placeholder="Isbn"
+        required
+        pattern="\d{10}|\d{13}"
+        title="Isbn Nummer kann 10 oder 13 Zeichen lang sein"
+        value={isbn}
+        onChange={(e) => {setIsbn(e.target.value)}} />
 
       <label>Erscheinungsdatum</label>
-      <input type="date" value={published} onChange={(e) => {setPublished(e.target.value)}} />
+      <input type="date" required value={published} onChange={(e) => {setPublished(e.target.value)}} />
 
       <label>Authoren</label>
       <button className="ui mini button" type="button" onClick={onAddAuthor}>+</button>
@@ -85,7 +92,11 @@ export default function BookForm(): JSX.Element {
       <div className="fields">
         {authors.map((author, index) =>
           <div key={index} className="sixteen wide field">
-            <input placeholder="Autor" value={author} onChange={(e) => {onChangeAuthor(e.target.value, index)}} />
+            <input
+              placeholder="Autor"
+              required
+              value={author}
+              onChange={(e) => {onChangeAuthor(e.target.value, index)}} />
           </div>
         )}
       </div>
@@ -97,12 +108,12 @@ export default function BookForm(): JSX.Element {
       <button className="ui mini button" type="button" onClick={onAddThumbnail}>+</button>
       {thumbnails.map((thumbnail, index) =>
         <div key={index} className="field">
-          <input placeholder="Url" className="eight wide field" value={thumbnail.url}
+          <input placeholder="Url" required className="eight wide field" value={thumbnail.url}
             onChange={(e) => {onChangeThumbnail(index, {url: e.target.value})}} />
           <input placeholder="Titel" className="seven wide field" value={thumbnail.title}
             onChange={(e) => {onChangeThumbnail(index, {title: e.target.value})}} />
           <button className="ui mini button" type="button" onClick={() => onRemoveThumbnail(index)}>-</button>
-        </div>
+        </div >
       )}
       <button className="ui button">Submit</button>
     </form >